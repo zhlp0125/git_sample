aaa78625aa4100ace0f8af1130110a3af16b0072
zhlp0125 <lipeng0125@163.com>
2014-06-05 14:01:35 +0800

    Update test.java

--- src/git_sample/test.java
+++ src/git_sample/test.java
@@ -1,9 +1,9 @@
 package git_sample;
 
 public class test {
-	public static void main(String[] args) {
-		
-	}
+  public static void main(String[] args) {
+    System.out.println("Main method.");
+  }
   public void run() {
     System.out.println("Run method.");
   }
