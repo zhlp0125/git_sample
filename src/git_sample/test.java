a8de8715a0d05f1d4c3676e8830ca003e8ef359d
zhanglipeng <lipengzhang@anjuke.com>
2014-06-05 14:30:57 +0800

    Commit

--- src/git_sample/test.java
+++ src/git_sample/test.java
@@ -4,7 +4,8 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("Main method.");
 	}
-  public void run() {
-    System.out.println("Run method.");
-  }
+
+	public void run() {
+		System.out.println("Run method.");
+	}
 }
