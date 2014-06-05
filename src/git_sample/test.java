44111aefce91e073fc54320b3d3eca925ec32cf6
zhanglipeng <lipengzhang@anjuke.com>
2014-06-05 15:25:04 +0800

    branch1

--- src/git_sample/test.java
+++ src/git_sample/test.java
@@ -2,6 +2,6 @@ package git_sample;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println("Main method.[branch1]-->[branch2]");
+		System.out.println("Main method.[branch1]-->[branch2]-->[merge to branch1]");
 	}
 }
