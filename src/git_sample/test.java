2c0fdb29eb757c9136849b36e8f4276de80ae676
zhanglipeng <lipengzhang@anjuke.com>
2014-06-05 15:27:18 +0800

    master

--- src/git_sample/test.java
+++ src/git_sample/test.java
@@ -2,10 +2,10 @@ package git_sample;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println("Main method.[master]");
+		System.out.println("Main method.[master].");
 	}
 
 	public void run() {
-		System.out.println("Run method.[master]");
+		System.out.println("Run method.[master].");
 	}
 }
