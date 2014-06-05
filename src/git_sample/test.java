dec2def6498a2de3c0830e9635cb71ab27463a74
zhanglipeng <lipengzhang@anjuke.com>
2014-06-05 15:43:09 +0800

    main

--- src/git_sample/test.java
+++ src/git_sample/test.java
@@ -3,6 +3,7 @@ package git_sample;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("Main method.[master].");
+		System.out.println("Main method.[master].new");
 	}
 
 	public void run() {
