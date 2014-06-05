3b76272f3ad84011114e8c2bbf0fef9e813d2d68
zhanglipeng <lipengzhang@anjuke.com>
2014-06-05 14:31:59 +0800

    commit master

--- src/git_sample/test.java
+++ src/git_sample/test.java
@@ -2,10 +2,10 @@ package git_sample;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println("Main method.");
+		System.out.println("Main method.[master]");
 	}
 
 	public void run() {
-		System.out.println("Run method.");
+		System.out.println("Run method.[master]");
 	}
 }
